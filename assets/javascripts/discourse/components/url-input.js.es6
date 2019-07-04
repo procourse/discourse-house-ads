b529d8b779279f821f7087583325b2f54e80838b
Sudaraka Jayathilaka <sudaraka@circles.asia>
2019-07-04 22:53:44 +0800

    FEATURE: Add ads-category-plugin

---
+++ assets/javascripts/discourse/components/url-input.js.es6
@@ -0,0 +1,15 @@
+import  Category from "discourse/models/category";
+import { default as computed } from "ember-addons/ember-computed-decorators";
+
+export default Ember.Component.extend({
+  @computed("model.categoryId")
+  showField(){
+    const categoryId = this.get("model.categoryId");
+    var category = Category.findById(categoryId);
+
+    if (category && category.name.toLowerCase() == Discourse.SiteSettings.ads_category_name.toLowerCase()) {
+      return true;
+    }
+    return false;
+  },
+});
---
+++ plugin.rb
@@ -0,0 +1,43 @@
+# name: ads-house-ads
+# authors: ProCourse Team
+# version: 0.1
+# url: https://github.com/procourse/discourse-house-ads.git
+
+register_asset 'stylesheets/ads-category.scss'
+
+after_initialize do
+  Topic.register_custom_field_type('ad_url', :string)
+  add_to_class(:topic, :ad_url) { self.custom_fields['ad_url'] }
+  add_to_serializer(:topic_view, :ad_url) { object.topic.ad_url }
+
+  Topic.register_custom_field_type('show_images', :boolean)
+  add_to_class(:topic, :show_images) { self.custom_fields['show_images'] }
+  add_to_serializer(:topic_view, :show_images) { object.topic.show_images }
+
+  TopicList.preloaded_custom_fields << 'show_images' if TopicList.respond_to? :preloaded_custom_fields
+  add_to_serializer(:topic_list_item, :show_images) { object.show_images }
+
+  TopicList.preloaded_custom_fields << 'ad_url' if TopicList.respond_to? :preloaded_custom_fields
+  add_to_serializer(:topic_list_item, :ad_url) { object.ad_url }
+
+  PostRevisor.track_topic_field(:ad_url)
+
+  PostRevisor.track_topic_field(:show_images)
+
+  PostRevisor.class_eval do
+    track_topic_field(:show_images) do |tc, show_images|
+      tc.record_change('show_images', tc.topic.custom_fields['show_images'], show_images)
+      tc.topic.custom_fields['show_images'] = show_images
+    end
+
+    track_topic_field(:ad_url) do |tc, ad_url|
+      tc.record_change('ad_url', tc.topic.custom_fields['ad_url'], ad_url)
+      tc.topic.custom_fields['ad_url'] = ad_url
+    end
+  end
+
+  DiscourseEvent.on(:before_create_topic) do |topic, creator|
+    topic.custom_fields['ad_url'] = creator.opts['ad_url']
+    topic.custom_fields['show_images'] = creator.opts['show_images']
+  end
+end
